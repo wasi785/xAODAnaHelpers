ecfdbf56813d28454fb03a4c044e518bd98c932f
Giordon Stark <kratsg@gmail.com>
2020-11-13 10:01:59 -0500

    remove postExecute

--- xAODAnaHelpers/OverlapRemover.h
+++ xAODAnaHelpers/OverlapRemover.h
@@ -250,7 +250,6 @@ public:
   virtual StatusCode changeInput (bool firstFile);
   virtual StatusCode initialize ();
   virtual StatusCode execute ();
-  virtual StatusCode postExecute ();
   virtual StatusCode finalize ();
   virtual StatusCode histFinalize ();
 
